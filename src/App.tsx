1cb824079c1279c8975d4abcba6ea3caa1470101
Rafael Poveda <raerpo@gmail.com>
2019-01-27 17:13:09 -0300

    feat: add styled components. remove dist from git

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
 import * as React from "react";
+import styled, { createGlobalStyle } from 'styled-components';
 
 import { useHNTopStories, useHNStoriesData } from "./api";
 import Story from './components/Story';
 
+const GlobalStyles = createGlobalStyle`
+  body {
+    background-color: #f6f6ef;
+    font-family: 'Open Sans', sans-serif;
+    font-size: 16px;
+    margin: 0px;
+  }
+`
+
+const Titlebar = styled.div`
+  background-color: #ff6600;
+  padding: .3rem .5rem;
+  h2 {
+    font-size: 0.9rem;
+    margin: 0px;
+    padding: 0px;
+  }
+`
+
 const App = () => {
   const { loading, stories } = useHNTopStories();
-  const storiesData = useHNStoriesData(stories, 0, 10);
+  const storiesData = useHNStoriesData(stories, 0, 20);
   return (
     <div>
-      <h2>Hackernew hooks</h2>
+      <GlobalStyles />
+      <Titlebar>
+        <h2>Hackernews hooks</h2>
+      </Titlebar>
       {loading && <p>Getting stories...</p>}
       {storiesData && (
         <ul>
